a4f36581fce62548afa3de205a300f2e14ab6085
SrinathAlla18 <68918857+SrinathAlla18@users.noreply.github.com>
2023-07-22 23:25:31 -0500

    Rabbitmq b/w customer, Notification.

--- customer/src/main/java/com/srinath/customer/CustomerApplication.java
+++ customer/src/main/java/com/srinath/customer/CustomerApplication.java
@@ -4,7 +4,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages={
+                "com.srinath.customer",
+                "com.srinath.amqp",
+        }
+)
 @EnableEurekaClient
 @EnableFeignClients(
         basePackages = "com.srinath.clients"
--- customer/src/main/java/com/srinath/customer/CustomerService.java
+++ customer/src/main/java/com/srinath/customer/CustomerService.java
@@ -1,14 +1,17 @@
 package com.srinath.customer;
 
+import com.srinath.amqp.RabbitMQMessageProducer;
 import com.srinath.clients.fraud.FraudCheckResponse;
 import com.srinath.clients.fraud.FraudClient;
 import com.srinath.clients.notification.NotificationClient;
 import com.srinath.clients.notification.NotificationRequest;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@AllArgsConstructor
 public class CustomerService {
     @Autowired
     private CustomerRepository customerRepository;
@@ -16,8 +19,9 @@ public class CustomerService {
     private RestTemplate restTemplate;
     @Autowired
     private FraudClient fraudClient;
-    @Autowired
-    private NotificationClient notificationClient;
+    private final RabbitMQMessageProducer rabbitMQMessageProducer;
+
+
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer= Customer.builder()
                 .firstName(request.firstName())
@@ -38,14 +42,14 @@ public class CustomerService {
         if (fraudCheckResponse.isFraudster()){
             throw new IllegalStateException("Fraudster");
         }
-        // todo : make it async. i.e add to queue.
         NotificationRequest notificationRequest = new NotificationRequest(
                 customer.getId(),
                 customer.getEmail(),
-                "Thank you for registering!"
-        );
-
-        notificationClient.sendNotification(notificationRequest);
+                "Thank you for registering!");
+        rabbitMQMessageProducer.publish(
+                notificationRequest,
+                "internal.exchange",
+                "internal.notification.routing-key");
 
     }
 }
